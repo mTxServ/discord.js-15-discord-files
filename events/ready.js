59131977b8cdc5e9a33739e3e9be7bee87894b97
Seb <sdieunidou@users.noreply.github.com>
2020-11-15 00:28:58 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,9 @@
+const Discord = require('discord.js')
+
 module.exports = {
     run: (client) => {
         client.logger.log('info', `Bot identifié en tant que ${client.user.tag}! (${client.user.id})`);
+
+        client.setTimeout(() => client.user.setActivity(`${client.guilds.cache.size} serveur(s)`, { type: 'LISTENING' }), 5000 * 60) // mise à jour du message de présence toutes les 5 minutes
     }
 };
