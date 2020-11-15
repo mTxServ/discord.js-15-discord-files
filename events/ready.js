217299f98b45efe409749efa5bf6fc207767df31
Seb <sdieunidou@users.noreply.github.com>
2020-11-15 13:27:13 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,10 @@
-const Discord = require('discord.js')
-
 module.exports = {
     run: (client) => {
         client.logger.log('info', `Bot identifié en tant que ${client.user.tag}! (${client.user.id})`);
 
-        const updateActivity = () {
-              client.user.setActivity(`${client.guilds.cache.size} serveur(s)`, { type: 'LISTENING' }), 5000 * 60) 
-        };
+        const updateActivity = () => {
+            client.user.setActivity(`${client.guilds.cache.size} serveur(s)`, { type: 'LISTENING' })
+        }
 
         updateActivity(); // le bot est 'ready', on initialise le message d'activité. Ensuite, le setInterval() permettra de rafraîchir le message toutes les 5 minutes
 
